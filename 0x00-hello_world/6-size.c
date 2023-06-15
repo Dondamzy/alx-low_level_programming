f9816d584fbe54d2702a1a4643b0731041f4bedc
Dondamzy <dondamzy99@gmail.com>
2023-06-15 15:28:56 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - A porgram that prints the sizes of various size on the computer
  *  Return: 0 (Success)
- *  */
+ */
 int main(void)
 {
 	char a;
@@ -12,7 +12,7 @@ int main(void)
 	float e;
 
 	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
