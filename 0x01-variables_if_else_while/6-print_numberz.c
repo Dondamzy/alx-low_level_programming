fe8d0a5e1e00d89a8d81de985a4669f3662ce31c
Dondamzy <dondamzy99@gmail.com>
2023-06-17 13:32:24 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,16 @@
 /**
  * main - Entry point
  * Description -' program that prints all single digit numbers'
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-
+	for (i = 48; i < 58; i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
