6e2dae4971c982298af81e5932e42ee670f0a743
Dondamzy <dondamzy99@gmail.com>
2023-06-17 07:41:19 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - A program that prints 'alphabets in lowercase and uppercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar('A' + i);
+	}
+	putchar('\n');
+return (0);
+}
